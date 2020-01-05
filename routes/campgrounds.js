05a291b7cbbb7b4eb87c0749be28bb730d49ea17
ankit-0355 <prajapatiankits810@gmail.com>
2020-01-05 21:05:43 +0530

    Add edit and Delete routes

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -49,7 +49,7 @@ router.get("/:id", function(req, res){
 	});
 });
 
-router.get("/:id/edit", function(req, res){
+router.get("/:id/edit", checkCampgroundOwnership, function(req, res){
 	Campground.findById(req.params.id, function(err, foundCampground){
 		if(err) {
 			console.log(err);
@@ -59,7 +59,7 @@ router.get("/:id/edit", function(req, res){
 	});
 });
 
-router.put("/:id", function(req, res){
+router.put("/:id", checkCampgroundOwnership, function(req, res){
 	Campground.findByIdAndUpdate(req.params.id, {$set: req.body.campground}, function(err, updatedcampground){
 		if(err){
 			console.log(err);
@@ -70,7 +70,7 @@ router.put("/:id", function(req, res){
 	});
 });
 
-router.delete("/:id", function(req, res){
+router.delete("/:id", checkCampgroundOwnership, function(req, res){
 	Campground.findByIdAndRemove(req.params.id, function(err){
 		if(err) {
 			console.log(err);
@@ -86,6 +86,25 @@ function IsLoggedIn(req, res, next){
 		return next();
 	}
 	res.redirect("/login");
+}
+
+function checkCampgroundOwnership (req, res, next){
+	if(req.isAuthenticated()){
+		Campground.findById(req.params.id, function(err, foundCampground){
+			if(err) {
+				console.log(err);
+				res.redirect("back");
+			} else {
+				if(foundCampground.author.id.equals(req.user._id)) {
+					next();
+				} else {
+					res.redirect("back");
+				}
+			}
+		});
+	} else {
+		res.redirect("back");
+	}
 }
 
 module.exports = router;
--- routes/comments.js
+++ routes/comments.js
@@ -37,11 +37,41 @@ router.post("/", IsLoggedIn, function(req, res){
 	});
 });
 
+router.delete("/:comment_id", checkCommentOwnership, function(req, res){
+	Comment.findByIdAndRemove(req.params.comment_id, function(err){
+		if(err) {
+			console.log(err);
+			res.redirect("/campground");
+		} else {
+			res.redirect("/campground/" + req.params.id);
+		}
+	});
+});
+
 function IsLoggedIn(req, res, next){
 	if(req.isAuthenticated()){
 		return next();
 	}
 	res.redirect("/login");
+}
+
+function checkCommentOwnership (req, res, next){
+	if(req.isAuthenticated()){
+		Comment.findById(req.params.comment_id, function(err, foundComment){
+			if(err) {
+				console.log(err);
+				res.redirect("back");
+			} else {
+				if(foundComment.author.id.equals(req.user._id)) {
+					next();
+				} else {
+					res.redirect("back");
+				}
+			}
+		});
+	} else {
+		res.redirect("back");
+	}
 }
 
 module.exports = router;
